a5b688ebff96e6cecb4993d0e0a8b428e1aef107
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-10-12 21:46:36 +0200

    s

--- profilUzytkownika.php
+++ profilUzytkownika.php
@@ -16,10 +16,12 @@
 </head>
 
 <body>
-    <header class="sticky-header-top">
-        <a href="index.php"><h1 class="header-title">Praktykuj.edu.pl</h1></a>
-        <div class="header-buttons-top">
+    <!-- Nagłówek, który pojawia się po przewinięciu -->
+    <header class="sticky-header">
+        <h1 class="header-title">Praktykuj.edu.pl</h1>
+        <div class="header-buttons">
             <a href="logowanie.php" class="cta-button login-button">Zaloguj się</a>
+            <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
             <a href="#" class="cta-button secondary browse-button">Przeglądaj ogłoszenia</a>
         </div>
     </header>
