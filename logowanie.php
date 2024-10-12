f8952c5639c7e15bd62fa32e5cf775c260660334
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-10-12 16:28:39 +0200

    logowanieCssComplete

--- logowanie.php
+++ logowanie.php
@@ -5,18 +5,21 @@
 <!DOCTYPE HTML>
 <html lang="pl">
 <head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<link rel="stylesheet" href="logowanieStyle.css">
+	<meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>praktykuj.edu.pl</title>
+
+	<link rel="stylesheet" type="text/css" href="logowanieStyle.css">
 </head>
 
 <body>
 	<div id="container">
-		<form action="zaloguj.php" method="post">
+		<form action="zaloguj.php" method="post" autocomplete="off">
 			<label><input name="login" type="text" placeholder="email" onfocus="this.placeholder=''" onblur="this.placeholder='email'"/></label>
 			<label><input name="haslo" type="password" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'"/></label>
 			<input type="submit" value="Zaloguj się" />
+			<div class="bladLogowania">Błędny login lub hasło!</div>
 		</form>
 	</div>
 </body>
