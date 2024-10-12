67c3d787a13e0b403747cba8e58774c6a11f6fe7
S1iwa <michalsliwa04@gmail.com>
2024-10-12 15:32:01 +0200

    test

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="pl">
 
 <head>
-    <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
@@ -12,7 +11,18 @@
 </head>
 
 <body>
-    <h1>Praktykuj.edu.pl</h1>
+    <main class="center-content">
+        <h1 class="title">Praktykuj.edu.pl</h1>
+        <p class="subtitle">Practice makes perfect</p>
+        <section class="action-buttons">
+            <a href="#" class="cta-button">Zaloguj się</a>
+            <a href="#" class="cta-button secondary">Przeglądaj ogłoszenia</a>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Wszelkie prawa zastrzeżone.</p>
+    </footer>
 </body>
 
 </html>
