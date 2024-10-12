925cf08ef4e4f69f269c38f627cfa45449ac5745
JGTB0PL <jgtb0pl@gmail.com>
2024-10-12 20:18:26 +0200

    -

--- show_ogloszenia.php
+++ show_ogloszenia.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, dziedzina, pensja, stanowisko, `data`, zdjecie, nazwa FROM ogloszenie o JOIN pracodawca p ON o.NIP_Pracodawca = p.NIP WHERE o.dziedzina LIKE '".$keyword."%' LIMIT ".$entries_count;
+$sql = "SELECT id, dziedzina, pensja, stanowisko, `data`, logo, nazwa FROM ogloszenie o JOIN pracodawca p ON o.NIP_Pracodawca = p.NIP WHERE o.dziedzina LIKE '".$keyword."%' LIMIT ".$entries_count;
 $result = $conn->query($sql);
 ?>
 <style>
@@ -55,7 +55,7 @@ body {
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             echo '<div class="item">
-                <img src="data:image/jpeg;base64,' . base64_encode($row["zdjecie"]) . '" alt="' . $row["stanowisko"] . '" class="item-image" />
+                <img src="data:image/jpeg;base64,' . base64_encode($row["logo"]) . '" alt="' . $row["stanowisko"] . '" class="item-image" />
                 <div class="item-content">
                     <h2>' . $row["stanowisko"] . '</h2>
                     <h3>' . $row['nazwa'] . '</h3>
