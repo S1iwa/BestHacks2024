5ad07993697d2c3d8b3f4735dac6186fab87bfab
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-10-12 21:43:42 +0200

    profilUzytkownika

---
+++ profilUzytkownika.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>praktykuj.edu.pl</title>
+
+	<link rel="stylesheet" type="text/css" href="profilStyle.css">	
+    <link rel="stylesheet" type="text/css" href="header.css">	
+
+</head>
+
+<body>
+    <header class="sticky-header-top">
+        <a href="index.php"><h1 class="header-title">Praktykuj.edu.pl</h1></a>
+        <div class="header-buttons-top">
+            <a href="logowanie.php" class="cta-button login-button">Zaloguj się</a>
+            <a href="#" class="cta-button secondary browse-button">Przeglądaj ogłoszenia</a>
+        </div>
+    </header>
+
+	<div id="container">
+        <div class="title">Profil użytkownika</div>
+	</div>
+</body>
+</html>
