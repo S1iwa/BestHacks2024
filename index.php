7bfe9361c345d38e28975501596b7cc400ae16e4
S1iwa <michalsliwa04@gmail.com>
2024-10-12 14:26:08 +0200

    test

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-<?php
-    echo "\nIch bin Anna aus Cleves *Ja?* \nWhen he saw me portrait he was like *Ja!*";
-    echo "\nBut I didn't look as good as I did in my pic";
-    echo "\nFunny how we all discuss this but never Henry's little di--";
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+    <meta name="description" content="Opis strony"/>
+    <meta name="keywords" content="słowa kluczowe oddzielone przecinkiem"/>
+    <title>Praktykuj.edu.pl</title>
+</head>
+
+<body>
+    <h1>Praktykuj.edu.pl</h1>
+</body>
+
+</html>
