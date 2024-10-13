5350293738c99919a2a2cfd67073c1e4aade78c9
JGTB0PL <jgtb0pl@gmail.com>
2024-10-13 03:32:14 +0200

    logowanie

--- profilUzytkownika.php
+++ profilUzytkownika.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['user_id']) && isset($_SESSION['user_type'])) {
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_type'])) {
     header("Location: /h/logowanie.php");
     exit();
 }
